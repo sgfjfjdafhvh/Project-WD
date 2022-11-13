ac4f4baa05b5b70821e289e31495790adb1a2053
Muhammad6196 <65651659+Muhammad6196@users.noreply.github.com>
2022-11-13 19:28:50 +0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,8 +2,10 @@ if game.PlaceId == 6284583030 or game.PlaceId == 7722306047 or game.PlaceId == 1
     loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/scripts/PSX.lua"))()
 elseif game.PlaceId == 9872472334 or game.PlaceId == 10324346056 or game.PlaceId == 10662542523 or game.PlaceId == 10324347967 or game.PlaceId == 10539706691 or game.PlaceId == 10808838353 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/scripts/Evade.lua"))()
-elseif game.PlaceId == 8888615802 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TR1V5/TR1V5-Software-V4/main/Games/Rainbow%20Friends.lua"))()
+elseif game.PlaceId == 9551640993 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/scripts/MS2.lua"))()
 elseif game.PlaceId == 6839171747 or game.PlaceId == 6516141723 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/scripts/Doors.lua"))()
+elseif game.PlaceId == 10675066724 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/scripts/SlimeTycoon.lua"))()
 end
