f6341fbdcaebeddba817dd6d3f3e7066413928c4
sgfjfjdafhvh <145346210+sgfjfjdafhvh@users.noreply.github.com>
2023-09-18 19:27:19 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/Mainstring.lua"))()
+loadstring(game:HttpGet("https://raw.githubusarcontent.com/Muhammad6196/Project-WD/main/Mainstring.lua"))()
